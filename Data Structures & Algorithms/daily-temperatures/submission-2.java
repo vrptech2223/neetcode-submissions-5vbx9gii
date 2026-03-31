21a23c409d7d0d04df09bc00e399361bef847dd3
vrptech2223 <vrptech2223@gmail.com>
2026-03-31 13:07:47 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> s = new Stack();
+        int[] res = new int[temperatures.length];
+        for(int i=0;i < temperatures.length ; i++)
+        {
+            while (!s.isEmpty() && temperatures[i] > temperatures[s.peek()])
+            {
+                int j = s.pop();
+                int r = i - j ;
+                res[j] = r;
+            }
+                s.push(i);
+        
+
+            System.out.println(s);
+        }
+
+        return res;
+        
+    }
+}
