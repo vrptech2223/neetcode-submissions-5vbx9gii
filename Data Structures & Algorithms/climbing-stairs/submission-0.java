f5036b823ace18258113e846bebae6fe653de099
vrptech2223 <vrptech2223@gmail.com>
2026-04-14 09:40:15 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,16 @@
+public class Solution {
+    int[] cache;
+    public int climbStairs(int n) {
+        cache = new int[n];
+        for (int i = 0; i < n; i++) {
+            cache[i] = -1;
+        }
+        return dfs(n, 0);
+    }
+
+    public int dfs(int n, int i) {
+        if (i >= n) return i == n ? 1 : 0;
+        if (cache[i] != -1) return cache[i];
+        return cache[i] = dfs(n, i + 1) + dfs(n, i + 2);
+    }
+}
